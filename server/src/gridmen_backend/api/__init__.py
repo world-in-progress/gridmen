076ea087621ec1893a6f23650066e74dad5d5f58
Flittly <2605948957@qq.com>
2026-01-16 16:58:48 +0800

    Refactor code structure for improved readability and maintainability

--- server/src/gridmen_backend/api/__init__.py
+++ server/src/gridmen_backend/api/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from .endpoints import schema, proj, patch, vector
+from .endpoints import schema, proj, patch, vector, grid
 
 api_router = APIRouter(prefix='/api')
 api_router.include_router(schema.router)
 api_router.include_router(proj.router)
 api_router.include_router(patch.router)
 api_router.include_router(vector.router)
+api_router.include_router(grid.router)
